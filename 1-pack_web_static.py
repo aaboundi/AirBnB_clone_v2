eba1b6b92d87e4a16ecfcb2ed9f2e3ce5156def7
aaboundi <aadja@gmail.com>
2022-09-29 13:22:55 -0700

    E111 indentation is not a multiple of 4

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,11 +7,11 @@ from datetime import datetime
 
 
 def do_pack():
-     """Function to compress files"""
-     local("mkdir -p versions")
-     result = local("tar -cvzf versions/web_static_{}.tgz web_static"
-                    .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
-                    capture=True)
-     if result.failed:
-         return None
-     return result
+    """Function to compress files"""
+    local("mkdir -p versions")
+    result = local("tar -cvzf versions/web_static_{}.tgz web_static"
+                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
+                   capture=True)
+    if result.failed:
+        return None
+    return result
