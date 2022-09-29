9661cb816a782d3172dba874abd1b04feee85d79
aaboundi <aadja@gmail.com>
2022-09-29 13:01:54 -0700

    Error-sudo

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,7 +12,7 @@ sudo echo "<html>
        <body>
          ALX School
        <body>
-</html>" > /data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
