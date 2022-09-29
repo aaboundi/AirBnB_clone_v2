19a266122967b353fdc1f6a5e1ec365c382bf24c
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-09-29 21:03:46 +0000

    Deploy_static_2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -25,4 +25,27 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """function to distribute an archive to a server"""
+    """Deploys the static files to the host servers.
+    Args:
+        archive_path (str): The path to the archived static files.
+    """
+    if not os.path.exists(archive_path):
+        return False
+    file_name = os.path.basename(archive_path)
+    folder_name = file_name.replace(".tgz", "")
+    folder_path = "/data/web_static/releases/{}/".format(folder_name)
+    success = False
+    try:
+        put(archive_path, "/tmp/{}".format(file_name))
+        run("mkdir -p {}".format(folder_path))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
+        run("rm -rf /tmp/{}".format(file_name))
+        run("mv {}web_static/* {}".format(folder_path, folder_path))
+        run("rm -rf {}web_static".format(folder_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(folder_path))
+        print('New version deployed!')
+        success = True
+    except Exception:
+        success = False
+    return success
