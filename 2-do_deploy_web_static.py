fac6d7ba97939344e81d8a0695e1ec5da1ad10d4
aaboundi <aadja@gmail.com>
2022-10-04 04:30:18 -0700

    static-2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
-"""Generates a .tgz archive from the
-contents of the web_static folder
-Distributes an archive to a web server"""
+"""Fabfile to distrubute an archive to a web server"""
 
-from fabric.operations import local, run, put
+from fabric.api import env, run, put
 from datetime import datetime
-import os
-from fabric.api import env
-import re
+from os.path import isfile
 
 
 env.hosts = ['44.200.176.121', '44.200.77.205']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
 
 
-def do_pack():
-    """Function to compress files"""
+"""def do_pack():
     local("mkdir -p versions")
     result = local("tar -cvzf versions/web_static_{}.tgz web_static"
                    .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
@@ -24,6 +17,7 @@ def do_pack():
     if result.failed:
         return None
     return result
+"""
 
 
 def do_deploy(archive_path):
@@ -31,23 +25,26 @@ def do_deploy(archive_path):
     Args:
         archive_path (str): The path to the archived static files.
     """
-    if not os.path.exists(archive_path):
+    if not isfile(archive_path):
         return False
-    file_name = os.path.basename(archive_path)
-    folder_name = file_name.replace(".tgz", "")
-    folder_path = "/data/web_static/releases/{}/".format(folder_name)
-    success = False
     try:
-        put(archive_path, "/tmp/{}".format(file_name))
-        run("sudo mkdir -p {}".format(folder_path))
-        run("sudo tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
+        file_name = archive_path.split('/')[1]
+        folder_name = file_name.split('.')[0]
+        folder_path = "/data/web_static/releases/"
+
+        put(archive_path, "/tmp/".format(file_name))
+        run("sudo mkdir -p {}{}".format(folder_path, folder_name))
+        run("sudo tar -xzf /tmp/{} -C {}{}".format(file_name, folder_path,
+                                                   folder_name))
         run("sudo rm -rf /tmp/{}".format(file_name))
-        run("sudo mv {}web_static/* {}".format(folder_path, folder_path))
-        run("sudo rm -rf {}web_static".format(folder_path))
+        run("sudo mv {0}{1}/web_static/* {0}{1}/".format(folder_path,
+                                                         folder_name))
+        run("sudo rm -rf {}{}/web_static".format(folder_path, folder_name))
         run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(folder_path))
+        run("sudo ln -s {}{} /data/web_static/current".format(folder_path,
+                                                              folder_name))
         print('New version deployed!')
         success = True
     except Exception:
         success = False
-    return success
+    return sucess
